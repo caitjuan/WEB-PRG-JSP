88b5f6275d4d49e186a5d8be62ae02d737b24a8b
Caitlienne Diane Juan <caitliennedianejuan@gmail.com>
2018-03-20 00:21:39 +0800

    Revert "ayoko na"
    This reverts commit 67872092f2a4bd6ef53b57b24ea49ac51fa26aa1.

--- DIYminimalist/test/uploadImage.java
+++ DIYminimalist/test/uploadImage.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@WebServlet(urlPatterns = {"/uploadImage"})
+@WebServlet(urlPatterns={"/uploadImage"})
 public class uploadImage extends HttpServlet {
     
     private static final String SAVE_DIR = "images";
